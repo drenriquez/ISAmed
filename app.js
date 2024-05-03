e9773660bbf5c0c0fca960bb3a8ee552b211bf8d
drenriquez <enriconotaro@gmail.com>
2024-05-03 12:11:24 +0200

    added logout system

--- app.js
+++ app.js
@@ -6,33 +6,46 @@ const path = require('path');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');//middleware per generare file di log
+const { v4: uuidv4 } = require('uuid');// generatore di identificatore univoco universale, è un numero a 128 bit utilizzato per identificare individualmente i dati
+
+const { MongoClient } = require('mongodb');
+const dbURI='';
+//const mongoClient= new MongoClient(dbURI, { useUnifiedTopology: true});
+
 const rfs = require('rotating-file-stream') // version 2.x
 const helmet= require('helmet')// libreria middleware per Node.js che aiuta a proteggere le tue applicazioni Express impostando vari header HTTP correlati alla sicurezza
 const homeRouter = require('./routes/home');
 const usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
-
+const logoutRouter = require('./routes/logout');
 const app = express();
+
 
 app.use(helmet());
 app.use(cookieParser());
+//app.use(sessionMiddleware);
 app.use(session({
-  secret: 'your-secret-key',
+  secret: 'your-secret-key',//USARE VARIABILI DI AMBIENTE
   resave: false,
   saveUninitialized: false,
-  cookie: { maxAge: 3600000 } // Durata della sessione in millisecondi (1 ora)
+  cookie: { maxAge: 3600000 }, // Durata della sessione in millisecondi (1 ora)
+  genid:()=> uuidv4(),
+  //store:
 }));
+
 // Funzione per eliminare le sessioni scadute
-function cleanExpiredSessions() {
-  app.locals.sessions.forEach((session, sessionId) => {
-    if (session.expires < Date.now()) {
-      // Rimuovi la sessione scaduta
-      delete app.locals.sessions[sessionId];
-    }
-  });
-}
+
+// function cleanExpiredSessions() {
+//   app.locals.sessions.forEach((session, sessionId) => {
+//     if (session.expires < Date.now()) {
+//       // Rimuovi la sessione scaduta
+//       delete app.locals.sessions[sessionId];
+//     }
+//   });
+// }
 // Esegui la pulizia delle sessioni scadute ogni minuto
-setInterval(cleanExpiredSessions, 60000); // Ogni minuto (60000 millisecondi)
+
+//setInterval(cleanExpiredSessions, 60000); // Ogni minuto (60000 millisecondi)
 
 //-------- FFILE DI LOG -create a write stream (in append mode)-------------------------------------
 // create a rotating write stream
@@ -40,7 +53,7 @@ var accessLogStream = rfs.createStream('access.log', {
   interval: '1d', // rotate daily
   path: path.join(__dirname, 'logs')
 })
-var accessLogStream = fs.createWriteStream(path.join(__dirname,'logs', 'access.log'), { flags: 'a' })
+//var accessLogStream = fs.createWriteStream(path.join(__dirname,'logs', 'access.log'), { flags: 'a' })
 // setup the logger
 app.use(morgan('combined', { stream: accessLogStream }))
 //----------------------------------------------------------------------------------------------------
@@ -51,7 +64,6 @@ app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // Servi i file statici dalla cartella 'node_modules'
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
@@ -59,6 +71,7 @@ app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 app.use(homeRouter);
 app.use(usersRouter);
 app.use(loginRouter);
+app.use(logoutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/logout.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const { userAuth } = require('../middleware/userAuth');
+
+/* GET home page. */
+router.get('/logout',userAuth, function(req, res, next) {
+    req.session.isAuthenticated=false
+    req.session.destroy;
+    res.redirect('/login')
+    console.log(req.session);
+});
+
+module.exports = router;
